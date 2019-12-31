3e14c5a3b1cae176c7fdd3713bf48315a0087ba2
Alexandru Turcanu <alexandru_turcanu@ymail.com>
2019-12-31 12:22:14 +0200

    Added Autonomous Section

--- StonkScorer/ContentView.swift
+++ StonkScorer/ContentView.swift
@@ -9,8 +9,67 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var foundationRepositioned = false
+
+    @State private var numberOfSkystoneBonuses = 0
+    @State private var skystoneBonusOptions = [0, 1, 2]
+
+    @State private var stonesDelivered = 0
+    @State private var stonesPlaced = 0
+
+
+    @State private var numberOfNavigationOptions = 0
+    @State private var navigatingOptions = ["None", "One", "Both"]
+
     var body: some View {
-        Text("Hello, World!")
+        NavigationView {
+            List {
+                Section(header: Text("Autonomous").font(.headline)) {
+                    Toggle(isOn: $foundationRepositioned) {
+                        Text("Repositioning")
+                    }
+
+                    HStack(spacing: 56) {
+                        Text("Skystone Bonus")
+                        Picker(selection: $numberOfSkystoneBonuses, label: Text("Skystone Bonus")) {
+                            ForEach(0 ..< skystoneBonusOptions.count) {
+                                Text("\(self.skystoneBonusOptions[$0])")
+                            }
+                        }.pickerStyle(SegmentedPickerStyle())
+                    }
+
+                    Stepper("Stones Delivered: \(stonesDelivered)",
+                        value: $stonesDelivered,
+                        in: 0...6
+                    )
+
+                    Stepper("Stones Placed: \(stonesPlaced)",
+                        value: $stonesPlaced,
+                        in: 0...6
+                    )
+
+                    HStack(spacing: 56) {
+                        Text("Navigating")
+                        Picker(selection: $numberOfNavigationOptions, label: Text("Navigating")) {
+                            ForEach(0 ..< navigatingOptions.count) {
+                                Text(self.navigatingOptions[$0])
+                            }
+                        }.pickerStyle(SegmentedPickerStyle())
+                    }
+
+                    HStack() {
+                        Text("Total Pts")
+                        Text("56")
+                            .frame(minWidth: 0, maxWidth: .infinity, alignment: .trailing)
+                            .padding(.trailing)
+                    }
+                }
+            }
+            .listStyle(GroupedListStyle())
+            .environment(\.horizontalSizeClass, .regular)
+            .navigationBarTitle("Scorer")
+        }
+
     }
 }
 
