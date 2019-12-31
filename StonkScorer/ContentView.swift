801ce0e817359710211c113389aa6b2232ca0eff
Alexandru Turcanu <alexandru_turcanu@ymail.com>
2019-12-31 12:12:59 +0200

    Initial Commit

---
+++ StonkScorer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  StonkScorer
+//
+//  Created by Alexandru Turcanu on 31/12/2019.
+//  Copyright © 2019 CodingBytes. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
