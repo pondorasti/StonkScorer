d560b0666740d2be22865f6926b0f76c9ccec196
Pondorasti <alexandru_turcanu@ymail.com>
2020-01-05 00:38:25 +0200

    Minor Improvements
    + launch screen

--- StonkScorer/Splash Screen/FeaturesContainerView.swift
+++ StonkScorer/Splash Screen/FeaturesContainerView.swift
@@ -13,11 +13,23 @@ struct FeaturesContainerView: View {
 
     var body: some View {
         VStack(alignment: .leading, spacing: 16) {
-            FeatureInfoView(title: "User Focused App", description: "Intuitive user interfaces and seamless transitions.", imageName: "person.crop.square.fill")
+            FeatureInfoView(
+                title: "User Focused App",
+                description: "Intuitive user interfaces and seamless transitions.",
+                imageName: "person.crop.square.fill"
+            )
 
-            FeatureInfoView(title: "Multi Platform Support", description: "Use the app on any device you would like. iPhone. iPad. Mac.", imageName: "aspectratio.fill")
+            FeatureInfoView(
+                title: "Multi Platform Support",
+                description: "Use the app on any device you would like. iPhone. iPad. Mac.",
+                imageName: "aspectratio.fill"
+            )
 
-            FeatureInfoView(title: "Dark Mode Support", description: "Automatically switches between Light and Dark Mode based on your time.", imageName: "circle.righthalf.fill")
+            FeatureInfoView(
+                title: "Dark Mode Support",
+                description: "Automatically switches between Light and Dark Mode based on your time.",
+                imageName: "circle.righthalf.fill"
+            )
         }
         .padding(.horizontal)
     }
