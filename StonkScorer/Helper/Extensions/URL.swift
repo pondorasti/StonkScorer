3e7395e078586df06cf4e26f5ce005d8efde0b32
Pondorasti <alexandru_turcanu@ymail.com>
2020-01-27 09:42:09 +0200

    Handling App Store IDs for iOS & macOS

--- StonkScorer/Helper/Extensions/URL.swift
+++ StonkScorer/Helper/Extensions/URL.swift
@@ -10,11 +10,25 @@ import Foundation
 import UIKit
 
 extension URL {
+    private static let iOSID = "id1493387167"
+    private static let macOSID = "id1496183214"
+
+
     enum appLinks: String {
+        #if os(macOS)
+        case appStore = "itms-apps://itunes.apple.com/app/id1496183214"
+        case rateApp = "itms-apps://itunes.apple.com/app/id1496183214?action=write-review"
+        #endif
+
+        #if os(iOS)
         case appStore = "itms-apps://itunes.apple.com/app/id1493387167"
         case rateApp = "itms-apps://itunes.apple.com/app/id1493387167?action=write-review"
+        #endif
+
         case twitter = "https://twitter.com/Pondorasti"
         case privacyPolicy = "https://docs.google.com/document/d/1Pf2aK4PXcrWsGKXQau_ivqZhgamcuvLsd9jmFtxyN44/edit?usp=sharing"
+
+        
     }
 
     init?(link: appLinks) {
